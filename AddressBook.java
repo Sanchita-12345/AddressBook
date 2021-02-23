83bfa20ee1231ebcb913c87dca0f3506e884b66f
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-02-23 22:27:53 +0530

    Editing the Person's Details in AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class AddressBook
 {
+	//Add contacts
 	static PersonDetails getDataFromConsole() {
-
+		
 		Scanner in = new Scanner(System.in);
-
+		
 		System.out.println("Enter the First name : ");
 		String firstname = in.next();
 		System.out.println("Enter the Last name : ");
@@ -25,21 +26,48 @@ public class AddressBook
 
 		return new PersonDetails(firstname, lastname, address, city, state, pin, phoneNumber);
 	}
+	
+	//Edit Person's Details
+	static PersonDetails editPersonDetails(PersonDetails pd) {
+		Scanner in = new Scanner(System.in);
 
+		System.out.println("Enter the Address : ");
+		pd.address = in.next();
+		System.out.println("Enter the City : ");
+		pd.city = in.next();
+		System.out.println("Enter the State : ");
+		pd.state = in.next();
+		System.out.println("Enter the Pin");
+		pd.pin = in.next();
+		System.out.println("Enter the contact number...");
+		pd.phoneNumber = in.next();
+		return pd;
+
+	}
 	public static void main(String args[])
 	{
 		PersonDetails pd = getDataFromConsole();
 		int mychoice;
-
+		
 		 while(true)
 		 {
 			Scanner in = new Scanner(System.in);
-			System.out.println("Enter your choice \n 1: For Adding person");
+			System.out.println("Enter your choice \n 1: For Adding person \n 2:For Editing person's details");
 			mychoice = in.nextInt();
 			switch (mychoice)
 			{
 				case 1:
 					pd = getDataFromConsole();
+					break;
+					
+				case 2:
+					System.out.println("Enter the person's First name : ");
+					String fName = in.next();
+					if(fName.equals(pd.firstname))
+						pd = editPersonDetails(pd);
+					else
+						System.out.println(fName + " First name is no valid ");
+					System.out.println(pd);
 					break;
 				default:
 					System.out.println("Invalid Input");
--- PersonDetails.java
+++ PersonDetails.java
@@ -9,7 +9,7 @@ public class PersonDetails {
 	public String state;
 	public String pin;
 	public String phoneNumber;
-
+	
 	PersonDetails(String firstname, String lastname, String address, String city, String state, String  pin, String phoneNumber) {
 
 		this.firstname = firstname;
@@ -20,10 +20,10 @@ public class PersonDetails {
 		this.pin = pin;
 		this.phoneNumber = phoneNumber;
 	}
-
+	
 	@Override
 	public String toString() {
-		return "PersonInformation [FirstName : " + firstname + ", LastName : " + lastname + ", Address : " + address +
+		return "PersonInformation [FirstName : " + firstname + ", LastName : " + lastname + ", Address : " + address + 
 				", city : " + city + ", state : " + state + ", PinCode : " + pin + ", PhoneNumber : " + phoneNumber + "]";
 	}
 }
