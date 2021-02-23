83454eac745d6d1e6d81971ad6085cb98e712c6a
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-02-23 22:04:28 +0530

    Creating contact in AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,42 @@
 package pack2;
-class AddressBook{
-	public static void main(String args[])
-	{
-		System.out.println("Welcome to AddressBook Program");
-	}
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AddressBook
+{
+		//variables
+		private String firstname;
+		private String lastname;
+		private String address;
+		private String city;
+		private String state;
+		private String pin;
+		private String phoneNumber;
+		private String email;
+
+		//constructor
+		public AddressBook(String firstname, String lastname, String address, String city, String state, String  pin, String phoneNumber, String email) {
+
+			this.firstname = firstname;
+			this.lastname = lastname;
+			this.address = address;
+			this.city = city;
+			this.state = state;
+			this.pin = pin;
+			this.phoneNumber = phoneNumber;
+			this.email=email;
+		}
+
+		// Printing all the information
+		public void display() {
+			System.out.println(" First Name : " + firstname + "\n Last Name : " + lastname + "\n Address : " + address +
+									"\n City : " + city + "\n State : " + state + "\n Pin Code : " + pin +
+									"\n Phone Number : " + phoneNumber + "\n Email id : " + email);
+		}
+		public static void main(String args[])
+		{
+			System.out.println("Welcome to the AddressBook Program");
+			AddressBook obj=new AddressBook( "Sanchita" , "Barik" , "Chandrakona Town" , "West Medinipur" , "West Bengal" , "721201" , "9933637905" , "sanchitabarik40@gmail.com" );
+			obj.display();
+		}
 }
