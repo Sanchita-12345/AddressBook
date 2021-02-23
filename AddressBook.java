162ab8a0c04e7288037a8402aa1640b81d08ca38
Sanchita-12345 <sanchitabarik20@gmail.com>
2021-02-23 22:14:16 +0530

    Adding contact in AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -4,39 +4,46 @@ import java.util.Scanner;
 
 public class AddressBook
 {
-		//variables
-		private String firstname;
-		private String lastname;
-		private String address;
-		private String city;
-		private String state;
-		private String pin;
-		private String phoneNumber;
-		private String email;
+	static PersonDetails getDataFromConsole() {
 
-		//constructor
-		public AddressBook(String firstname, String lastname, String address, String city, String state, String  pin, String phoneNumber, String email) {
+		Scanner in = new Scanner(System.in);
 
-			this.firstname = firstname;
-			this.lastname = lastname;
-			this.address = address;
-			this.city = city;
-			this.state = state;
-			this.pin = pin;
-			this.phoneNumber = phoneNumber;
-			this.email=email;
-		}
+		System.out.println("Enter the First name : ");
+		String firstname = in.next();
+		System.out.println("Enter the Last name : ");
+		String lastname = in.next();
+		System.out.println("Enter the Address : ");
+		String address = in.next();
+		System.out.println("Enter the City : ");
+		String city = in.next();
+		System.out.println("Enter the State : ");
+		String state = in.next();
+		System.out.println("Enter the Pin : ");
+		String pin = in.next();
+		System.out.println("Enter the contact number : ");
+		String phoneNumber = in.next();
 
-		// Printing all the information
-		public void display() {
-			System.out.println(" First Name : " + firstname + "\n Last Name : " + lastname + "\n Address : " + address +
-									"\n City : " + city + "\n State : " + state + "\n Pin Code : " + pin +
-									"\n Phone Number : " + phoneNumber + "\n Email id : " + email);
+		return new PersonDetails(firstname, lastname, address, city, state, pin, phoneNumber);
+	}
+
+	public static void main(String args[])
+	{
+		PersonDetails pd = getDataFromConsole();
+		int mychoice;
+
+		 while(true)
+		 {
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter your choice \n 1: For Adding person");
+			mychoice = in.nextInt();
+			switch (mychoice)
+			{
+				case 1:
+					pd = getDataFromConsole();
+					break;
+				default:
+					System.out.println("Invalid Input");
+			}
 		}
-		public static void main(String args[])
-		{
-			System.out.println("Welcome to the AddressBook Program");
-			AddressBook obj=new AddressBook( "Sanchita" , "Barik" , "Chandrakona Town" , "West Medinipur" , "West Bengal" , "721201" , "9933637905" , "sanchitabarik40@gmail.com" );
-			obj.display();
-		}
+	}
 }
